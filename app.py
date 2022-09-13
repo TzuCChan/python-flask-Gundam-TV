24ce746aa5a9e9a2933d3f0dc662014b2e9ce332
Leon Tzu Chiang Chan <leontzuchiangchan@Leons-Air.lan>
2022-09-13 11:29:00 -0400

    Update 4

--- app.py
+++ app.py
@@ -4,3 +4,23 @@ from flask import Flask, request, jasonify
 from peewee import *
 from playhouse.shortcuts import model_to_dict, dict_to_model
 from playhouse.postgres_ext import ArrayField
+
+db = PostgresqlDatabase('ms', user='leontzuchiangchan',
+                        password='', host='localhost', port=5432)
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Title(BaseModel):
+    name = CharField()
+    year = IntegerField()
+    mobile_suit = ArrayField(CharField)
+    protagonist = CharField()
+
+
+db.connect()
+db.drop_tables([Title])
+db.create_tables([Title])
